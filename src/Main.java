fab107e9eee5e3819cd01392ec926f0753d05202
zeynabseyfatova <zseyfatova@gmail.com>
2023-02-07 20:41:50 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Clarusway Team!");
+        System.out.print("I created first java project and pushed to Github");
+    }
+}
